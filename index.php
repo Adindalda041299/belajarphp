29d8269f38a4e9bb331bdd64c1d9b8c98617cfb4
Adindalda041299 <72003012+Adindalda041299@users.noreply.github.com>
2021-01-04 11:26:23 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+require_once "db.php";
+
+
+$sql = "SELECT * FROM mahasiswa";
+
+$hasil = $conn->query($sql);
+
+?>
+
+<table border="1" cellpadding="3">
+<tr>
+<th>Nomor</th>
+<th>NIM</th>
+<th>Nama</th>
+<th>Alamat</th>
+</tr>
+
+<?php
+
+$nomor = 0;
+while($row = $hasil->fetch_assoc())
+{
+    $nomor++;
+?>
+<tr>
+<td><?=$nomor;?></td>
+<td><?=$row['NIM'];?></td>
+<td><?=$row['Nama'];?></td>
+<td><?=$row['Alamat'];?></td>
+<td>
+<a href="update.php?Nim=<?=$row['NIM'];?>">Update</a>
+<a href="process_delete.php">Hapus</a>
+</td>
+</tr>
+<?php
+}
+?>
+</table>
+<a href="create.php">Tambah Data</a>
